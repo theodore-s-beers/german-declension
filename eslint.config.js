f864a12dd2b0aacdf66c7b9d65b99740f8b1dba0
Theo Beers <32523293+theodore-s-beers@users.noreply.github.com>
2024-05-08 15:09:27 +0200

    Reorganize ESLint config

--- eslint.config.js
+++ eslint.config.js
@@ -2,15 +2,17 @@ import js from '@eslint/js'
 
 export default [
   {
-    ignores: ['**/*.min.js'],
+    ignores: ['**/*.min.js']
+  },
 
+  js.configs.recommended,
+
+  {
     languageOptions: {
       globals: {
         console: 'readonly',
         document: 'readonly'
       }
-    },
-
-    ...js.configs.recommended
+    }
   }
 ]
